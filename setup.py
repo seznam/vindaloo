6c0cc8074c4cdfb8098299042f37c1fb67e4482a
Daniel Milde <daniel.milde@firma.seznam.cz>
2019-09-13 10:48:26 +0200

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='vindaloo',
-    version='3.0.0',
+    version='3.0.1',
     description='K8S deployer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seznam/vindaloo",
     author='Daniel Milde',
     author_email='daniel.milde@firma.seznam.cz',
     install_requires=[
@@ -19,4 +26,5 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
+    python_requires='>=3.5',
 )
