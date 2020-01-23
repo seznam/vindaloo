9f8665bf0311c7bde63e2b6a4bb694b1cee12676
Viktor Lacina <viktor.lacina@firma.seznam.cz>
2020-01-23 15:11:45 +0100

    next version :-)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='vindaloo',
-    version='3.2.0',
+    version='3.2.1',
     description='K8S deployer',
     long_description=long_description,
     long_description_content_type="text/markdown",
